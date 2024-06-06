f01ba12a9f99f3d059de6d0ca64f3a4a7873ab24
AnnaP-G <klientkv1511@gmail.com>
2024-06-06 14:19:26 +0300

    game

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,11 @@
 import showModal from './modal';
 
 export default function showWinnerModal(fighter) {
-    // call showModal function
+    const title = `${fighter.name} is the winner!`;
+    const bodyElement = document.createElement('div');
+    const fighterImage = document.createElement('img');
+    fighterImage.src = fighter.source;
+    bodyElement.appendChild(fighterImage);
+
+    showModal({ title, bodyElement });
 }
